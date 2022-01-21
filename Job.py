0034fa2f3c1cb23a0562775a6a2d34427a977629
biovino <biovino@gmail.com>
2022-01-21 12:45:49 -0500

    Working on Job.py and Manager.py

--- Job.py
+++ Job.py
@@ -1,13 +1,15 @@
 #
 # Ben Iovino
-# BIOL494, 1/19/22
+# BIOL494, 1/21/22
 # Job
 # This program looks through every fasta file in a directory and uses it
 # as an input for a program in the RNAstructure package.
+#
 
 
 # Import libraries
 import os
+import subprocess
 
 # Define a function that goes through a directory and makes a list of each
 # file in the directory.
@@ -26,19 +28,26 @@ def get_file_list(directory):
     return filelist
 
 # Define a function that performs a job on each file in the filelist
-def do_job(filelist):
+def do_job(filelist, directory):
 
-    python ../RNA/xios_from_rnastructure.py - i fasta - f g1 *.fa - w 1,5 - d 0,5 &>>param.log
+    # Use a for loop to iterate through each file in filelist
+    for file in filelist:
+
+        # Use subprocess to run Unix command
+        subprocess.Popen(['python /scratch/scholar/biovino/xios_from_rnastructure.py', '-i {}'.format(directory),
+                          '-f {}'.format(file), '-w 4', '-d 5', '&>>{}.xios'.format(file)])
 
 # Define the main function to ask for a directory path and run the job function
 def main():
 
-    # Prompt the user for directory path
-    directory = input("Enter full directory path: ")
-    print()
+    # Enter directory path
+    directory = "/scratch/scholar/biovino/RNAdata/Test"
 
     # Call the get_file_list function providing the directory path as a parameter
     filelist = get_file_list(directory)
+
+    # Call do_job function
+    do_job(filelist, directory)
 
 # End the main function
 main()
